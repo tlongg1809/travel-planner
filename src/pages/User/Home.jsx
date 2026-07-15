fdc8c49358262954c8d91e0e722b42ca271a7838
DoQuocDat12 <doquocdatmxst@gmail.com>
2026-07-15 11:51:16 +0700

    22222222

---
+++ src/pages/User/Home.jsx
@@ -0,0 +1,19 @@
+
+
+export default function Home() {
+  return (
+    <div className="flex">
+
+      <Sidebar />
+
+      <main className="flex-1 p-10 bg-gray-100 min-h-screen">
+
+        <h1 className="text-3xl font-bold">
+          Trang chủ
+        </h1>
+
+      </main>
+
+    </div>
+  );
+}
